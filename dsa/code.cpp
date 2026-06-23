5a255d36c7d8aacc24165679552a625f6454f1bd
Rachit <rachitpandey0603@gmail.com>
2026-06-23 23:24:10 +0530

    first dsa file

---
+++ dsa/code.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+
+// int a=10;
+// int *ptr =&a;
+// int **pt = &ptr;
+// cout<<ptr;
+// cout<<endl;
+// cout<<pt;
+// cout<<endl;
+// cout<<*ptr + 1;
+
+vector<int> vec1={1,2,3,4,5,6};
+cout<<vec1[2];
+
+return 0;
+}
+
+
+// Pointer Type	Description
+// Null Pointer	Points to nothing (nullptr)
+// Void Pointer	Can point to any data type
+// Wild Pointer	Uninitialized pointer
+// Dangling Pointer	Points to deleted memory
