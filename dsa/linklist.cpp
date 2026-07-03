5dc37d5614349d6d318ca11d437d899fe90e1025
Rachit <rachitpandey0603@gmail.com>
2026-07-03 23:58:38 +0530

    learning basics

---
+++ dsa/linklist.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node * next;
+
+    Node(int val){
+         data= val;
+         next=NULL;
+    }
+
+    void insert(Node* )
+    void print(Node* head){
+        while(temp->next)
+    }
+};
+int main(){
+
+ 
+
+    return 0;
+}
