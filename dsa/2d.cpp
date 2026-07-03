0f78d55bc033cfe50d86cd2bf88f7d12e6c44b94
Rachit <rachitpandey0603@gmail.com>
2026-07-03 23:50:21 +0530

    learning 2d aray

--- dsa/2d.cpp
+++ dsa/2d.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-void calculate(int arr[][3],int row){
+void calculate(int arr[][4],int row){
     
  for(int i =0;i<row;i++){ 
     int sum=0;   
@@ -9,13 +9,43 @@ void calculate(int arr[][3],int row){
  }cout<<sum<<endl;
 }
 }
+void maxcalculate(int arr[][4],int row){
+    int maxsum =0;
+    int r=0;
+ for(int i =0;i<row;i++){ 
+    int sum=0;   
+    for(int j =0;j<4;j++){
+       sum+=arr[i][j];
+ }
+if(sum>maxsum){
+    maxsum=sum;
+    r=i;
+} else if(sum == maxsum)
+        {
+            r = i - 1;   // previous row of the current row
+        }
+}cout<<" Max SUM is "<<maxsum<<" of row "<<r<<endl;
+}
+
 int main(){
 
-    int arr[3][3]={1,3,4
-                   ,6,7,4,
-                   1,3,7};
+    int arr[4][4]={6,7,472d,1
+                   ,6,7,47,1,
+                   6,7,4,1,
+                   6,7,47,1};
 
-                   calculate(arr,3);
+                   //calculate(arr,3);
+maxcalculate(arr,4);
+
+    // print value of max sum also its index
+    // if sum== max sum retunrn previous index 
+
+
+
+
+
+
+
 
     return 0;
 }
